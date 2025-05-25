407cceafe8a319e6b9042ef151bcac9443f41932
fredly11 <buechelewd@gmail.com>
2025-05-25 00:26:33 -0500

    recreated user group

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,14 +1,13 @@
 const awsmobile = {
   aws_project_region: 'us-east-2',
   aws_cognito_region: 'us-east-2',
-  aws_user_pools_id: 'us-east-2_YeMuomRAd',
+  aws_user_pools_id: 'us-east-2_73Cr7uTre',
   aws_user_pools_web_client_id: '6b0lule1v5ha6eb3a1pu9b370c',
   oauth: {},
   Auth: {
     mandatorySignIn: false,
     userAttributes: [
       { Name: 'email', Required: true },
-      { Name: 'preferred_username', Required: true },
       { Name: 'custom:email_updates', Required: false }
     ]
   }
