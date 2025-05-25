6f6e7ad57a39fb7e067b10bdb74ff08da6d1b3db
fredly11 <buechelewd@gmail.com>
2025-05-25 00:29:34 -0500

    fixed client id

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -2,7 +2,7 @@ const awsmobile = {
   aws_project_region: 'us-east-2',
   aws_cognito_region: 'us-east-2',
   aws_user_pools_id: 'us-east-2_73Cr7uTre',
-  aws_user_pools_web_client_id: '6b0lule1v5ha6eb3a1pu9b370c',
+  aws_user_pools_web_client_id: '5t0bf9mkeh7mc9f3smp1blv5l0',
   oauth: {},
   Auth: {
     mandatorySignIn: false,
